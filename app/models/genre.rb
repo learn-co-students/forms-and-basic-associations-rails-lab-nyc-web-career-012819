640db8135173808cfe9e46be1dca6b3d01e74965
Farhan <farhanmahmoodny@gmail.com>
2019-02-27 14:05:39 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,11 @@
 class Genre < ActiveRecord::Base
   # add associations
+  has_many :songs
+
+  # def song_genre_id=(ids)
+  #   ids.each do |id|
+  #     genre = Genre.find(id)
+  #     self.songs << genre
+  #   end
+  # end
 end
